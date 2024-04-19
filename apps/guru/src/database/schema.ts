9580dbcf1023a3146ff6fbfe65b2a2720ab9bd57
Abdullah Awan <abdullahawan7676@gmail.com>
2024-04-19 10:52:04 +0500

    fix: testcontainers installed

--- apps/guru/src/database/schema.ts
+++ apps/guru/src/database/schema.ts
@@ -1,11 +1,10 @@
-import { uuid, text, timestamp, pgTable, boolean } from 'drizzle-orm/pg-core';
+import { uuid, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
 import { type InferSelectModel } from 'drizzle-orm';
 
 export const Organisation = pgTable('organisation', {
   id: uuid('id').primaryKey(),
   region: text('region').notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
-  // the following properties are examples, it can removed / replaced to fit your use-case
   email: text('email').notNull(),
   token: text('token').notNull(),
 });
